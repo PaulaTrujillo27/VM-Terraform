0d7889aab36a2372ec74a8748e4f4012fc601014
Paula Andrea Trujillo <71205932+PaulaTrujillo27@users.noreply.github.com>
2024-03-19 03:51:40 +0000

    Virtual Machine Terraform

---
+++ variables.tf
@@ -0,0 +1,11 @@
+variable "vm_name" {
+  type        = string
+  description = "Virtual Machine Name"
+}
+
+
+variable "location" {
+  type        = string
+  default     = "West Europe"
+  description = "value"
+}
